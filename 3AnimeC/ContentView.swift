29ef9515f120477e29ee8d83a6995983227490c5
Adam <admay202@gmail.com>
2024-08-01 19:11:47 -0500

    Initial Commit

---
+++ 3AnimeC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  3AnimeC
+//
+//  Created by Adam May on 8/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 3AnimeC/_AnimeCApp.swift
@@ -0,0 +1,17 @@
+//
+//  _AnimeCApp.swift
+//  3AnimeC
+//
+//  Created by Adam May on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct _AnimeCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
